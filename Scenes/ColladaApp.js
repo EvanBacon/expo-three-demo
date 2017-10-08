c6557907c67cd5de9c90cff0a74dc8f7224c4cc9
Evan Bacon <baconbrix@gmail.com>
2017-10-08 12:09:42 -0700

    updating collada loading

--- Scenes/ColladaApp.js
+++ Scenes/ColladaApp.js
@@ -53,35 +53,83 @@ class App extends React.Component {
         const { innerWidth: width, innerHeight: height } = window;
         // Initialize Three.JS
 
-        const asset = Expo.Asset.fromModule(require('../assets/models/stormtrooper/stormtrooper.dae'));
+        this.scene.add(new THREE.GridHelper(10, 20));
+        const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+        this.scene.add(ambientLight);
+        const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+        directionalLight.position.set(1, 1, - 1);
+        this.scene.add(directionalLight);
+
+        await this._setupColladaModel();
+
+    }
+
+
+    _loadFile = async (localUri) => {
+        console.log("Load local file", localUri);
+        try {
+            const file = await Expo.FileSystem.readAsStringAsync(localUri);
+            return file;
+        } catch (error) {
+            console.log("Error from _loadFile");
+            console.error(error);
+        }
+    }
+
+
+    /* 
+           THREE.ColladaModel
+           
+           animations: any[];
+           kinematics: any;
+           scene: Scene;
+           library: any;
+    */
+    _loadColladaModel = async (staticResource) => {
+        //Load Asset
+        const asset = Expo.Asset.fromModule(staticResource);
         if (!asset.localUri) {
             await asset.downloadAsync();
         }
 
-        var loader = new THREE.ColladaLoader();
+
+        const loader = new THREE.ColladaLoader();
         loader.options.convertUpAxis = true;
         // loader.setCrossOrigin('assets/models/stormtrooper/');
-        loader.load(asset.localUri, (collada) => {
-            const animations = collada.animations;
-            const avatar = collada.scene;
-            this.mixer = new THREE.AnimationMixer(avatar);
-            const action = this.mixer.clipAction(animations[0]).play();
-            this.scene.add(avatar);
 
-            var helper = new THREE.SkeletonHelper( avatar );
-            helper.material.linewidth = 3;
-            this.scene.add( helper );
+        const file = await this._loadFile(asset.localUri);
+        // Cheever method (Dire Dire Ducks)
+        const collada = loader.parse(file);
+
+        // Alt Method: has onLoad function. Error signature may be different (less useful) than loading directly from FileSystem
+        // const collada = await (new Promise((res, rej) => loader.load(asset.localUri, res, () => {}, rej)));
+
+        return collada;
+    }
+
+    _setupColladaModel = async () => {
+        const collada = await this._loadColladaModel(require('../assets/models/stormtrooper/stormtrooper.dae'));
         
-        });
-        //
-        var gridHelper = new THREE.GridHelper(10, 20);
-        this.scene.add(gridHelper);
-        //
-        var ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
-        this.scene.add(ambientLight);
-        var directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
-        directionalLight.position.set(1, 1, - 1);
-        this.scene.add(directionalLight);
+        const {
+            animations,
+            kinematics,
+            scene: avatar,
+            library
+        } = collada;
+
+        /* 
+            Build a control to manage the animations
+            This breaks if the model doesn't have animations - this needs to be fixed as it's probably 80% of free models
+        */
+        this.mixer = new THREE.AnimationMixer(avatar);
+
+        const action = this.mixer.clipAction(animations[0]).play();
+
+        this.scene.add(avatar);
+
+        const helper = new THREE.SkeletonHelper(avatar);
+        helper.material.linewidth = 3;
+        this.scene.add(helper);
 
     }
 
@@ -102,7 +150,7 @@ class App extends React.Component {
         this.controls.update();
 
         if (this.mixer !== undefined) {
-            
+
             this.mixer.update(delta);
         }
 
